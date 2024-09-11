a6760a7f99d97092e482f45ddc75d9c5e8d71610
JU2363 <is04168@naver.com>
2024-09-11 14:15:44 +0900

    finished

---
+++ src/components/ViewBudget.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { formatNumberToWon } from "../utills";
+
+const ViewBudget = ({ budget, setIsEditing }) => {
+  return (
+    <>
+      <span>예산: {formatNumberToWon(budget)}</span>
+      <button
+        onClick={() => setIsEditing(true)}
+        type='button' 
+        className='btn btn-primary'
+      >
+        수정
+      </button>
+    </>
+  )
+}
+
+export default ViewBudget
